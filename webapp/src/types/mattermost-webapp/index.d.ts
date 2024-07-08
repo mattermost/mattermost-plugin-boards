874e52f6ff8ac9ae5b63315b39ed191515bcf458
Rajat Dabade <a-rajat.dabade@mattermost.com>
2024-07-08 12:36:36 +0530

    chore: typefix

--- webapp/src/types/mattermost-webapp/index.d.ts
+++ webapp/src/types/mattermost-webapp/index.d.ts
@@ -1,6 +1,9 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import React from 'react'
 import {Channel, ChannelMembership} from 'mattermost-redux/types/channels'
 
 export interface PluginRegistry {
