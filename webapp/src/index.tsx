6023e19d89f3de1937ddad600288838fba23f474
Rajat Dabade <a-rajat.dabade@mattermost.com>
2024-07-30 12:25:47 +0530

    chore: ci fix

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 import React, {useEffect} from 'react'
