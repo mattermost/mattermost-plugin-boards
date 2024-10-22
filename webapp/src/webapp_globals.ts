562db7d07fac0bb4a3ddb06dc6ad76b8eba61515
Rajat Dabade <a-rajat.dabade@mattermost.com>
2024-10-22 13:28:42 +0530

    refactor: fix this type for global PostUtil

--- webapp/src/webapp_globals.ts
+++ webapp/src/webapp_globals.ts
@@ -1,10 +1,32 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {NameMappedObjects} from "mattermost-redux/types/utilities"
+
+import {Channel} from "mattermost-redux/types/channels"
+
+import {Team} from "./store/teams"
+
+
+type Options = {
+    singleline: boolean;
+    atMentions: boolean;
+    mentionHighlight: boolean;
+    team: Team | null;
+    channelNamesMap: NameMappedObjects<Channel>;
+}
+
+export type PostUtils = {
+    formatText: (text: string, option: Options) => string;
+    messageHtmlToComponent: (html: string) => React.ReactNode;
+}
+
+export type MattermostWindow = {
+    PostUtils: PostUtils;
+}
+
+
 export const {
     formatText,
     messageHtmlToComponent,
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-} = global.PostUtils ?? {}
+} = (global as unknown as MattermostWindow).PostUtils ?? {}
