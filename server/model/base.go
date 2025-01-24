a6cbe9c0d7db6d642778c00cb8c23d0bfcb416fb
Harshil Sharma <harshilsharma63@gmail.com>
2025-01-24 10:13:34 +0530

    Added legacy ID check support

--- server/model/base.go
+++ server/model/base.go
@@ -2,10 +2,16 @@ package model
 
 import (
 	mmModel "github.com/mattermost/mattermost/server/public/model"
+	"regexp"
+	"strings"
 )
 
 const (
 	IdLength = 27
+
+	legacyIDLength        = 36
+	legacyIdPartSeparator = "-"
+	legacyIdPartCount     = 5
 )
 
 var (
@@ -18,6 +24,23 @@ func IsValidId(id string) error {
 		return errEmptyId
 	}
 
+	switch len(id) {
+	case IdLength:
+		{
+			return newIdCheck(id)
+		}
+	case legacyIDLength:
+		{
+			return legacyIdcCheck(id)
+		}
+	default:
+		{
+			return errInvalidId
+		}
+	}
+}
+
+func newIdCheck(id string) error {
 	// ID should have the right length
 	if len(id) != IdLength {
 		return errInvalidId
@@ -27,6 +50,47 @@ func IsValidId(id string) error {
 	// Excluding the first letter as it represents the block type
 	// and is not part of the format validation
 	if mmModel.IsValidId(id[1 : len(id)-1]) {
+		return errInvalidId
+	}
+
+	return nil
+}
+
+func legacyIdcCheck(id string) error {
+	// Check if the ID is empty
+	if id == "" {
+		return errInvalidId
+	}
+
+	// Check total length (exact 36 characters including hyphens)
+	if len(id) != 36 {
+		return errInvalidId
+	}
+
+	// Check hyphen positions
+	if id[8] != '-' || id[13] != '-' || id[18] != '-' || id[23] != '-' {
+		return errInvalidId
+	}
+
+	// Split the ID into segments based on hyphens
+	segments := strings.Split(id, "-")
+
+	// Validate each segment's length
+	if len(segments[0]) != 8 ||
+		len(segments[1]) != 4 ||
+		len(segments[2]) != 4 ||
+		len(segments[3]) != 4 ||
+		len(segments[4]) != 12 {
+		return errInvalidId
+	}
+
+	// Remove hyphens for character validation
+	cleanID := strings.ReplaceAll(id, "-", "")
+
+	// Regex to ensure only hexadecimal characters are used
+	match, _ := regexp.MatchString("^[0-9a-f]+$", cleanID)
+
+	if !match {
 		return errInvalidId
 	}
 
