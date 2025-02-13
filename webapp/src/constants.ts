6ed104a81a08fa1d64c7f1746fb66dd1cefec01b
Harshil Sharma <harshilsharma63@gmail.com>
2025-02-13 21:44:41 +0530

    Bumped plugin version to v9.1.2

--- webapp/src/constants.ts
+++ webapp/src/constants.ts
@@ -38,7 +38,7 @@ class Constants {
     static readonly titleColumnId = '__title'
     static readonly badgesColumnId = '__badges'
 
-    static readonly versionString = '9.1.1'
+    static readonly versionString = '9.1.2'
 
     static readonly archiveHelpPage = 'https://docs.mattermost.com/boards/migrate-to-boards.html'
     static readonly imports = [
