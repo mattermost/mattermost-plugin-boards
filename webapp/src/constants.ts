3e77f9a888d9d33692fe2786450f6cc7dd3be29b
Rajat Dabade <a-rajat.dabade@mattermost.com>
2025-06-18 15:24:05 +0530

    Updated boards version to v9.1.4 (#110)
    * Updated board version to v9.1.4
    
    * Updated snapshot

--- webapp/src/constants.ts
+++ webapp/src/constants.ts
@@ -38,7 +38,7 @@ class Constants {
     static readonly titleColumnId = '__title'
     static readonly badgesColumnId = '__badges'
 
-    static readonly versionString = '9.1.3'
+    static readonly versionString = '9.1.4'
 
     static readonly archiveHelpPage = 'https://docs.mattermost.com/boards/migrate-to-boards.html'
     static readonly imports = [
