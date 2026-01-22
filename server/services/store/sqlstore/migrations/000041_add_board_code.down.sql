b5eb84bc0139397be4b4896e6e26b4e433a85066
a-voronkov <alfred361@gmail.com>
2026-01-22 18:20:17 +0700

    Fix CI build issues
    - Update down migration to follow project convention (SELECT 1; only)
    - Add GetBoardByCode method to mockstore for tests

--- server/services/store/sqlstore/migrations/000041_add_board_code.down.sql
+++ server/services/store/sqlstore/migrations/000041_add_board_code.down.sql
@@ -1,10 +1,2 @@
-{{if .mysql}}
-DROP INDEX idx_boards_code ON {{.prefix}}boards;
-{{else}}
-DROP INDEX IF EXISTS {{.prefix}}idx_boards_code;
-{{end}}
-
-ALTER TABLE {{.prefix}}boards DROP COLUMN code;
-
-ALTER TABLE {{.prefix}}boards_history DROP COLUMN code;
+SELECT 1;
 
