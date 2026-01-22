9d401b494d57db3b7be49d00553629b8c4682b5b
a-voronkov <alfred361@gmail.com>
2026-01-23 00:13:23 +0700

    Fix

--- webapp/src/figmaUtils.ts
+++ webapp/src/figmaUtils.ts
@@ -1,8 +1,6 @@
 // Copyright (c) 2020-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {Utils} from './utils'
-
 export interface FigmaLinkInfo {
     url: string
     fileKey: string
