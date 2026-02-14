69695f6d21eac7296e8385dbffc116b7786fbc0c
Rajat Dabade <a-rajat.dabade@mattermost.com>
2026-02-14 16:50:37 +0530

    Clearing lastboard and view id after back to home btn clicked

--- webapp/src/pages/accessDeniedPage.tsx
+++ webapp/src/pages/accessDeniedPage.tsx
@@ -2,28 +2,59 @@
 // See LICENSE.txt for license information.
 
 import React, {useCallback} from 'react'
-import {useHistory} from 'react-router-dom'
+import {useHistory, useLocation} from 'react-router-dom'
 import {FormattedMessage, useIntl} from 'react-intl'
-
 
 import Button from '../widgets/buttons/button'
 import './accessDeniedPage.scss'
 
 import {setGlobalError} from '../store/globalError'
 import {useAppDispatch} from '../store/hooks'
+import {UserSettings} from '../userSettings'
 import AccessDeniedIllustration from '../svg/access-denied-illustation'
+
+const clearLastBoardAndViewIds = () => {
+    const lastTeamId = UserSettings.lastTeamId
+    if (lastTeamId) {
+        const lastBoardId = UserSettings.lastBoardId[lastTeamId]
+        if (lastBoardId) {
+            UserSettings.setLastViewId(lastBoardId, null)
+        }
+        UserSettings.setLastBoardID(lastTeamId, null)
+    }
+}
 
 const AccessDeniedPage = () => {
     const history = useHistory()
+    const location = useLocation()
     const intl = useIntl()
     const dispatch = useAppDispatch()
 
     const handleBackToHome = useCallback(() => {
         // Clear any error state before navigating
         dispatch(setGlobalError(''))
+
+        // Clear last board/view IDs as a precaution to prevent redirect loops.
+        const referrerPath = new URLSearchParams(location.search).get('r')
+        if (referrerPath) {
+            try {
+                const pathParts = decodeURIComponent(referrerPath).split('/').filter(Boolean)
+                if (pathParts[0] === 'team' && pathParts.length >= 3) {
+                    const teamId = pathParts[1]
+                    const boardId = pathParts[2]
+                    UserSettings.setLastBoardID(teamId, null)
+                    UserSettings.setLastViewId(boardId, null)
+                }
+            } catch {
+                clearLastBoardAndViewIds()
+            }
+        } else {
+            clearLastBoardAndViewIds()
+        }
+
         // Always navigate to home, not back to the board that caused the error
         history.push('/')
-    }, [history, dispatch])
+    }, [history, dispatch, location.search])
 
     return (
         <div className='AccessDeniedPage'>
