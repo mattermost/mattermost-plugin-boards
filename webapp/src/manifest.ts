622362ea3fc07cb11f24a740e4cbd59a03d89206
Nick Misasi <nick.misasi@mattermost.com>
2026-06-01 14:30:58 -0400

    Undo a manifest change that's breaking tests

--- webapp/src/manifest.ts
+++ webapp/src/manifest.ts
@@ -15,7 +15,7 @@ const manifest = JSON.parse(`
     "support_url": "https://github.com/mattermost/mattermost-plugin-boards/issues",
     "release_notes_url": "https://github.com/mattermost/mattermost-plugin-boards/releases/tag/v9.2.5",
     "icon_path": "assets/starter-template-icon.svg",
-    "version": "9.2.5",
+    "version": "0.0.0",
     "min_server_version": "11.7.3",
     "server": {
         "executables": {
